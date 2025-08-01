d1658ee9056ab0302d95c2d930b158f4db6702be
sirob <boris.pardo8021@utc.edu.ec>
2025-08-01 12:38:07 -0500

    Configuracion de EliminarComunidad

--- ReforestacionComunitaria/EliminarComunidad.aspx
+++ ReforestacionComunitaria/EliminarComunidad.aspx
@@ -1,16 +1,18 @@
 ﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="EliminarComunidad.aspx.vb" Inherits="ReforestacionComunitaria.EliminarComunidad" %>
 
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+    <title>Eliminar Comunidad</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
-        </div>
+        <h2>¿Estás seguro de que deseas eliminar esta comunidad?</h2>
+
+        <asp:Label ID="lblNombre" runat="server" Font-Bold="True"></asp:Label>
+        <br /><br />
+        <asp:Button ID="btnEliminar" runat="server" Text="Eliminar" OnClick="btnEliminar_Click" />
+        <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" PostBackUrl="ListaComunidad.aspx" />
     </form>
 </body>
 </html>
--- ReforestacionComunitaria/EliminarComunidad.aspx.vb
+++ ReforestacionComunitaria/EliminarComunidad.aspx.vb
@@ -1,8 +1,31 @@
-﻿Public Class EliminarComunidad
+﻿Imports ReforestacionComunitaria.ReforestacionComunitaria.Modelos
+
+
+Public Class EliminarComunidad
     Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            Dim idStr As String = Request.QueryString("id")
+            If Not String.IsNullOrEmpty(idStr) AndAlso IsNumeric(idStr) Then
+                Dim id As Integer = CInt(idStr)
+                Dim comunidad As ComunidadPa = ComunidadDAL.ObtenerPorId(id)
 
+                If comunidad IsNot Nothing Then
+                    lblNombre.Text = "Nombre: " & comunidad.Nombre
+                    ViewState("ComunidadId") = comunidad.Id
+                Else
+                    Response.Redirect("ListaComunidad.aspx")
+                End If
+            Else
+                Response.Redirect("ListaComunidad.aspx")
+            End If
+        End If
     End Sub
 
+    Protected Sub btnEliminar_Click(sender As Object, e As EventArgs)
+        Dim id As Integer = CInt(ViewState("ComunidadId"))
+        ComunidadDAL.Eliminar(id)
+        Response.Redirect("ListaComunidad.aspx")
+    End Sub
 End Class
